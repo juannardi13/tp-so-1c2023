4e8a03721b7077d55891f077ab87638243ef5699
juannardi13 <jnardi@frba.utn.edu.ar>
2023-05-25 06:07:38 +0000

    Cambios importantes en Consola

--- consola/main_consola.c
+++ consola/main_consola.c
@@ -3,43 +3,49 @@
 #include "../shared/include/main_shared.h"
 #include "../shared/src/main_shared.c"
 
-#define MAX_LEN 256
-
 int main(int argc, char** argv) {
+	// Inicialización de Consola
+
 	char* ip_kernel;
 	char* puerto_kernel;
-	FILE* archivo_pseudocodigo = NULL;
 
     t_log* logger = iniciar_logger();
 
     if(argc != 3){
-    		log_error(logger, "Uso: consola path tamanio");
+    		log_error(logger, "No se ha inicializado correctamente el módulo consola");
     		exit(1);
     }
 
     log_info(logger, "Hola! Se inicializo el modulo cliente Consola.");
+
+    //Lectura del archivo Config
 
     t_config* config = iniciar_config(argv[2]);
 
    	ip_kernel = config_get_string_value(config, "IP");
     puerto_kernel = config_get_string_value(config, "PUERTO");
 
-   // cargar_valores_config(logger, argv[2], ip_kernel, puerto_kernel);
-
 	log_info(logger, "Se ha leido el archivo de config con exito, con los valores PUERTO: %s e IP: %s.", puerto_kernel, ip_kernel);
 
-	archivo_pseudocodigo = abrir_archivo_instrucciones(argv[1], logger);
+	//Inicialización de Consola como cliente
 
     int conexion_kernel = crear_conexion(logger, "KERNEL", ip_kernel, puerto_kernel);
-
-    log_info(logger, "Conexion exitosa con el Kernel.");
 
     // Es para que consola espere nuestra aprobación para mandar las instrucciones. Al ejecutarlo va a frenarse, después de eso apretar ENTER
     getchar();
 
-    levantar_instrucciones(archivo_pseudocodigo, logger, conexion_kernel);
+    t_list *instrucciones = parsear_instrucciones(argv[1], logger);
+    t_paquete* paquete_de_instrucciones = crear_paquete();
+	serializar_instrucciones(instrucciones, paquete_de_instrucciones); //Esta función ya agrega al paquete las instrucciones.
 
-    return EXIT_SUCCESS;
+	//Envío de instrucciones a Kernel
+	log_warning(logger,"Cantidad de Instrucciones enviadas: %d", list_size(instrucciones));
+	enviar_paquete(paquete_de_instrucciones, conexion_kernel);
+	log_info(logger, "Paquete de instrucciones enviado correctamente!");
+
+	//Finalización de consola abajo...
+
+
+    return 0;
 }
-
 
