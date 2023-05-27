14452655f25a752092b1a1b7906629dd742eb4af
juannardi13 <jnardi@frba.utn.edu.ar>
2023-05-27 00:16:57 +0000

    Hago cambios para poder enviar también el tamaño del proceso, falta probarlo

--- consola/main_consola.c
+++ consola/main_consola.c
@@ -34,8 +34,16 @@ int main(int argc, char** argv) {
     // Es para que consola espere nuestra aprobación para mandar las instrucciones. Al ejecutarlo va a frenarse, después de eso apretar ENTER
     getchar();
 
+    //Creacion del paquete
+
+    int tamanio_proceso = atoi(argv[1]); //Calcula el peso del proceso para poder serializar y deserializar
+
+    t_paquete* paquete_de_instrucciones = crear_paquete_como(PAQUETE_CONSOLA);
+
+    agregar_a_paquete(paquete_instrucciones, &tamanio_proceso,sizeof(int)); //Agrega al paquete el tamanio del proceso como primer dato
+
     t_list *instrucciones = parsear_instrucciones(argv[1], logger);
-    t_paquete* paquete_de_instrucciones = crear_paquete();
+
 	serializar_instrucciones(instrucciones, paquete_de_instrucciones); //Esta función ya agrega al paquete las instrucciones.
 
 	//Envío de instrucciones a Kernel
