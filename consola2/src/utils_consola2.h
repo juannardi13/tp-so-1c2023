1aac9bed4a31d1afba504a163ddcb16623f9af9f
Juan Nardi <102627173+juannardi13@users.noreply.github.com>
2023-06-14 18:28:52 -0300

    Refactor y actualización utils_consola2.h

--- consola2/src/utils_consola2.h
+++ consola2/src/utils_consola2.h
@@ -14,18 +14,26 @@
 #include<readline/readline.h>
 #include<shared-2.h>
 
-t_log* iniciar_logger(void);
-t_config* iniciar_config(char*);
+
+//Iniciación estructuras (Logger, Config)
 FILE* abrir_archivo_instrucciones(char *, t_log*);
-void levantar_instrucciones(FILE*, t_log*, int);
 void cargar_valores_config(t_log*, char*, char*, char*);
+t_config* iniciar_config(char*);
+t_log* iniciar_logger(void);
+
+//Armado de Instrucciones con la estructura t_instruccion (Quedó obsoleto, optamos por usar char* para las instrucciones)
 t_instruccion* armar_instruccion(nombre_instruccion, char*, char*, char*);
 t_list* parsear_instrucciones(char*, t_log*);
+void parsear_instrucciones_y_enviar(char*, int, t_log);
 void serializar_instrucciones(t_list*, t_paquete*);
+
+//Apertura y lectura de Pseudocodigo desde un archivo .txt
 char* leer_archivo_pseudocodigo(char*, t_log*);
-void parsear_instrucciones_y_enviar(char* ruta_archivo_pseudocodigo, int socket_servidor, t_log* logger);
-void enviar_string(int conexion_servidor, char* string, op_code codigo_op);
-void* serializar_string(char* string, t_paquete* paquete, op_code codigo_op);
-int agregar_a_stream(void *stream, int* offset, void *src, int size);
+void levantar_instrucciones(FILE*, t_log*, int);
+
+//Serialización de las instrucciones como String y envío 
+int agregar_a_stream(void*, int*, void*, int);
+void enviar_string(int, char*, op_code);
+void* serializar_string(char*, t_paquete*, op_code);
 
 #endif /* UTILS_H_ */
