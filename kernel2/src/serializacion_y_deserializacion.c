5370898bd4c321cf15ac740a60dfca8446d02a13
juannardi13 <jnardi@frba.utn.edu.ar>
2023-06-15 19:55:45 +0000

    Agrego nuevo archivo para separar las funciones y que no quede todo junto

---
+++ kernel2/src/serializacion_y_deserializacion.c
@@ -0,0 +1,204 @@
+#include"utils_kernel2.h"
+#include<shared-2.h>
+
+//Deserealización de Strings, con esta función recibimos las instrucciones desde Consola
+char* deserializar_string(t_buffer* buffer) {
+	char* string = malloc(buffer->stream_size);
+	memset(string, 0, buffer->stream_size);
+
+	void* stream = buffer->stream;
+	memcpy(string, stream, buffer->stream_size);
+
+	printf("Las instrucciones que llegaron son: %s", string);
+
+	return string;
+}
+
+//Serialización y deserialización de PCBs
+void enviar_pcb(int socket_servidor, t_pcb* pcb) {
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+
+	buffer->stream_size = sizeof(int) * 4 + sizeof(estado_proceso) + sizeof(t_registros) + (strlen(pcb->instrucciones) + 1);
+
+	void* stream = malloc(buffer->stream_size);
+	int offset = 0;
+
+	memcpy(stream + offset, &(pcb->pid), sizeof(int));
+	offset += sizeof(int);
+	memcpy(stream + offset, &(pcb->tamanio_instrucciones), sizeof(int));
+	offset += sizeof(int);
+	memcpy(stream + offset, pcb->instrucciones, strlen(pcb->instrucciones) + 1);
+	offset += (strlen(pcb->instrucciones) + 1);
+	memcpy(stream + offset, &(pcb->pc), sizeof(estado_proceso));
+	offset += sizeof(estado_proceso);
+	memcpy(stream + offset, &(pcb->tamanio), sizeof(int));
+	offset += sizeof(int);
+	memcpy(stream + offset, &(pcb->registros), sizeof(t_registros));
+
+	buffer->stream = stream;
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+
+	paquete->codigo_operacion = PCB;
+	paquete->buffer = buffer;
+
+	void* a_enviar = malloc(buffer->stream_size + sizeof(uint8_t) + sizeof(uint32_t));
+	int desplazamiento = 0;
+
+	memcpy(a_enviar + desplazamiento, &(paquete->codigo_operacion), sizeof(uint8_t));
+	desplazamiento +=sizeof(uint8_t);
+	memcpy(a_enviar + desplazamiento, &(paquete->buffer->stream_size), sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+	memcpy(a_enviar + desplazamiento, paquete->buffer->stream, paquete->buffer->stream_size);
+
+	send(socket_servidor, a_enviar, buffer->stream_size + sizeof(uint8_t) + sizeof(uint32_t), 0);
+
+	free(a_enviar);
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+}
+
+t_pcb* recibir_pcb(int socket_servidor) {
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->buffer = malloc(sizeof(t_buffer));
+
+	recv(socket_servidor, &(paquete->codigo_operacion), sizeof(uint8_t), 0);
+
+	recv(socket_servidor, &(paquete->buffer->stream_size), sizeof(uint32_t), 0);
+	paquete->buffer->stream = malloc(paquete->buffer->stream_size);
+	recv(socket_servidor, paquete->buffer->stream, paquete->buffer->stream_size, 0);
+
+	switch(paquete->codigo_operacion) {
+	case PCB:
+		t_pcb* pcb = deserializar_pcb(paquete->buffer);
+		return pcb;
+	default:
+		printf("ERROR");
+		return 0;
+	}
+}
+
+t_pcb* deserializar_pcb(t_buffer* buffer) {
+	t_pcb* pcb = malloc(sizeof(t_pcb));
+
+	void* stream = buffer->stream;
+
+	memcpy(&(pcb->pid), stream, sizeof(int));
+	stream += sizeof(int);
+	memcpy(&(pcb->tamanio_instrucciones), stream, sizeof(int));
+	stream += sizeof(int);
+	pcb->instrucciones = malloc(pcb->tamanio_instrucciones);
+	memcpy(pcb->instrucciones, stream, pcb->tamanio_instrucciones);
+	stream += pcb->tamanio_instrucciones;
+	memcpy(&(pcb->estado), stream, sizeof(estado_proceso));
+	stream += sizeof(estado_proceso);
+	memcpy(&(pcb->pc), stream, sizeof(int));
+	stream += sizeof(int);
+	memcpy(&(pcb->tamanio), stream, sizeof(int));
+	stream += sizeof(int);
+	memcpy(&(pcb->registros), stream, sizeof(t_registros));
+
+	return pcb;
+}
+
+
+
+//Funciones de deserialización de estructuras que ya no se usan, como t_instruccion (NO ANDAN DEL TODO BIEN)
+t_list *deserializar_instrucciones(t_list *datos, int longitud_datos) {
+	t_list *instrucciones = list_create();
+
+
+  	for(int i = 0; i < longitud_datos; i += 4) {
+  		t_instruccion *instruccion_recibida = malloc(sizeof(t_instruccion));
+  		instruccion_recibida->nombre = *(op_code *)list_get(datos, i);
+  		instruccion_recibida->parametro_1 = list_get(datos, i + 1);
+  		instruccion_recibida->parametro_2 = list_get(datos, i + 2);
+  		instruccion_recibida->parametro_3 = list_get(datos, i + 3);
+  		printf("%d %s %s %s\n", instruccion_recibida->nombre, instruccion_recibida->parametro_1, instruccion_recibida->parametro_2, instruccion_recibida->parametro_3);
+  		list_add(instrucciones, instruccion_recibida);
+  	}
+
+  	return instrucciones;
+}
+
+t_consola *deserializar_consola(char* instrucciones, t_log* logger) {
+
+  	t_consola *consola = malloc(sizeof(t_consola));
+
+  	consola->tamanio_proceso = strlen(instrucciones);
+
+  	printf("\n El tamanio del proceso es: %d", consola->tamanio_proceso);
+
+  	consola->instrucciones = string_duplicate(instrucciones);//deserializar_instrucciones(datos, list_size(datos));
+
+  	free(instrucciones);
+
+  	return consola;
+}
+
+char* recibir_instrucciones_como_string(int socket_cliente, t_log* logger) {
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->buffer = malloc(sizeof(t_buffer));
+
+
+	recv(socket_cliente, &(paquete->buffer->stream_size), sizeof(uint32_t), 0);
+	paquete->buffer->stream = malloc(paquete->buffer->stream_size);
+	recv(socket_cliente, paquete->buffer->stream, paquete->buffer->stream_size, 0);
+
+	char* mensaje_recibido = malloc(paquete->buffer->stream_size);
+	void* stream = paquete->buffer->stream;
+
+	memcpy(mensaje_recibido, stream, paquete->buffer->stream_size);
+
+	mensaje_recibido = string_duplicate(paquete->buffer->stream);
+
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+
+	return mensaje_recibido;
+}
+
+void recibir_instruccion_serializada(int socket_cliente) {
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->buffer = malloc(sizeof(t_buffer));
+
+	recv(socket_cliente, &(paquete->buffer->stream_size), sizeof(uint32_t), 0);
+
+	paquete->buffer->stream = malloc(paquete->buffer->stream_size);
+
+	recv(socket_cliente, paquete->buffer->stream, paquete->buffer->stream_size, 0);
+
+	t_instruccion* instruccion_recibida = malloc(sizeof(t_instruccion));
+
+	void* stream = paquete->buffer->stream;
+	int desplazamiento = 0;
+
+	memcpy(&(instruccion_recibida->nombre), stream, sizeof(op_code));
+	stream += sizeof(op_code);
+	printf("\nLa isntruccion recibida fue: %d, ", instruccion_recibida->nombre);
+
+	memcpy(&(instruccion_recibida->parametro_1_length), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	memcpy(instruccion_recibida->parametro_1, stream, instruccion_recibida->parametro_1_length);
+	desplazamiento = (strlen(instruccion_recibida->parametro_1) + 1);
+	stream += desplazamiento;
+	printf("%s, ", instruccion_recibida->parametro_1);
+
+	memcpy(&(instruccion_recibida->parametro_2_length), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	memcpy(instruccion_recibida->parametro_2, stream, instruccion_recibida->parametro_2_length);
+	desplazamiento = (strlen(instruccion_recibida->parametro_2) + 1);
+	printf("El largo del string fue de %d, debería ser de %d", desplazamiento, instruccion_recibida->parametro_2_length);
+	stream += instruccion_recibida->parametro_2_length;
+
+	printf("%s, ", instruccion_recibida->parametro_2);
+	memcpy(&(instruccion_recibida->parametro_3_length), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	memcpy(instruccion_recibida->parametro_3, stream, instruccion_recibida->parametro_3_length);
+	printf("%s", instruccion_recibida->parametro_3);
+
+	printf("\nLa isntruccion recibida fue: %d, %s, %s, %s", instruccion_recibida->nombre, instruccion_recibida->parametro_1, instruccion_recibida->parametro_2, instruccion_recibida->parametro_3);
+}
