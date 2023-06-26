5878286442b583f98a28a6ee31ff93c912a7c402
Juan Nardi <102627173+juannardi13@users.noreply.github.com>
2023-06-25 23:29:49 -0300

    Actualizacion hilos_kernel2.c, saque funciones para simplificar el codigo
    Quizas tenga que cambiarle el nombre porque no se condice con lo que hacen las funciones del archivo

--- kernel2/src/hilos_kernel2.c
+++ kernel2/src/hilos_kernel2.c
@@ -60,7 +60,6 @@ void manejar_conexion(int socket_cliente) {
 		log_warning(logger_kernel, "Operacion desconocida \n");
 		break;
 	}
-
 }
 
 int atender_clientes_kernel(int socket_servidor) {
@@ -68,10 +67,10 @@ int atender_clientes_kernel(int socket_servidor) {
 	int socket_cliente = esperar_cliente(logger_kernel,"KERNEL", socket_servidor); // se conecta el cliente
 
 		if(socket_cliente != -1) {
-			//pthread_t hilo_cliente;
-			manejar_conexion(socket_cliente);
-			//pthread_create(&hilo_cliente, NULL, (void*) manejar_conexion, (void *) socket_cliente); // creo el hilo con la funcion manejar conexion a la que le paso el socket del cliente y sigo en la otra funcion
-			//pthread_detach(hilo_cliente);
+			pthread_t hilo_cliente;
+			//manejar_conexion(socket_cliente);
+			pthread_create(&hilo_cliente, NULL, (void*) manejar_conexion, (void *) socket_cliente); // creo el hilo con la funcion manejar conexion a la que le paso el socket del cliente y sigo en la otra funcion
+			pthread_detach(hilo_cliente);
 
 			return 1;
 		} else {
@@ -79,156 +78,6 @@ int atender_clientes_kernel(int socket_servidor) {
 		}
 
 	return 0;
-}
-
-void agregar_proceso_a_ready(void) {
-
-	if(list_size(cola_ready) <= config_kernel.grado_multiprogramacion) { // TODO Esto después tiene que ser implementado con semáforos contadores
-		t_proceso* proceso_auxiliar = malloc(sizeof(t_proceso));
-		proceso_auxiliar = list_remove(cola_new, 0);
-		list_add(cola_ready, proceso_auxiliar);
-
-		log_info(logger_kernel, "PCB id[%d] agregado a Cola de Ready", proceso_auxiliar->pcb->pid);
-		//free(proceso_auxiliar);
-	}
-
-	log_info(logger_kernel, "Cola de Ready: ");
-
-	for(int i = 0; i < list_size(cola_ready); i++) {
-		t_proceso* un_proceso = malloc(sizeof(t_proceso)) ;
-		un_proceso = list_get(cola_ready, i);
-		log_info(logger_kernel, "PCB id[%d]", un_proceso->pcb->pid);
-	}
-}
-
-void estado_ejecutar(void) {
-
-	while (1) {
-		sem_wait(&sem_exec);
-		pthread_mutex_lock(&mutex_exec);
-		t_proceso* proceso_a_ejecutar = list_get(cola_exec, 0); //esto ayuda para cuando se necesita devolver el mismo proceso a cpu, como en las instrucciones de memoria
-		pthread_mutex_unlock(&mutex_exec);
-
-		struct timespec inicio, final; //Estructuras y definiciones necesarias para calcular el HRRN
-		clock_gettime(CLOCK_REALTIME, &inicio);
-		long seconds; 
-		long nanoseconds;
-		double elapsed;
-		
-		enviar_pcb(socket_cpu, proceso->pcb);
-		log_info(logger_kernel, "PCB id[%d] enviada a CPU", proceso_a_ejecutar->pcb->pid);
-
-		op_code respuesta_cpu = recibir_operacion(socket_cpu);
-
-		proceso_a_ejecutar->pcb = recibir_pcb(socket_cpu);
-
-		switch (respuesta_cpu) {
-		case IO:
-			clock_gettime(CLOCK_REALTIME, &final);
-			seconds = final.tv_sec - inicio.tv_sec;
-			nanoseconds = final.tv_nsec - inicio.tv_nsec;
-			elapsed = seconds + nanoseconds * 1e - 9;
-			proceso_a_ejecutar->ultima_rafaga = elapsed;
-			
-			//TODO buscar la manera de ejecutar IO
-			//Quizas convenga mandarlo a dormir con usleep en estado block
-			//Tambien ver si conviene crear la funcion get_time();
-			//Otra forma de hacerlo puede ser:
-
-			struct timespec inicio_bloqueo;
-			clock_gettime(CLOCK_REALTIME, &inicio_bloqueo);
-			long segundos_inicio_bloqueo = inicio_bloqueo.tv_sec;
-			long nanosegundos_inicio_bloqueo = inicio_bloqueo.tv_nsec;
-			double comienzo_bloqueo = segundos_inicio_bloqueo + nanosegundos_inicio_bloqueo * 1e - 9;
-			proceso_a_ejecutar->inicio_block = comienzo_bloqueo;
-
-			//Una forma de resolverlo:
-			proceso_a_ejecutar->tiempo_bloqueo = recibir_parametro_instruccion(); //Implementar esta fuincion y fijarse si es en microsegundos nanosegundos o lo que sea
-
-			pthread_mutex_lock(&mutex_block_io);
-			proceso_a_ejecutar->pcb->estado = BLOCK;
-			list_add(cola_block_io, proceso_a_ejecutar);
-			pthread_mutex_unlock(&mutex_block_io);
-
-			sem_post(&sem_block_io);
-			
-			log_warning(logger_kernel, "La última instrucción ejecutada fue IO");
-			break;
-		case F_OPEN:
-			//Abrir o crear el archivo pasado por parámetro
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_OPEN");
-			break;
-		case F_CLOSE:
-			//Cerrar el archivo pasado por parámetro
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_CLOSE");
-			break;
-		case F_SEEK:
-			//Actualizar el puntero del archivo a la posición pasada por parámetro
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_SEEK");
-			break;
-		case F_READ:
-			//Leer del archivo indicado la cantidad de bytes pasados por parámetro y escribirlo en la dirección física de memoria
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_READ");
-			break;
-		case F_WRITE:
-			//Esta instrucción solicita al Kernel que se escriba en el archivo indicado la cantidad de bytes pasados por parámetro cuya información es obtenida a partir de la dirección física de Memoria.
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_WRITE");
-			break;
-		case F_TRUNCATE:
-			//Modificar el tamaño del archivo al indicado por parámetro.
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue F_TRUNCATE");
-			break;
-		case WAIT:
-			//Se asigne una instancia del recurso indicado por parámetro
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue WAIT");
-			break;
-		case SIGNAL:
-			//Libera una instancia del recurso indicado por parámetro.
-			log_warning(logger_kernel,
-					"La última instrucción ejecutada fue SIGNAL");
-			break;
-		case YIELD:
-			//Se pone en cola de ready al proceso de nuevo.
-			clock_gettime(CLOCK_REALTIME, &final);
-			seconds = final.tv_sec - inicio.tv_sec;
-			nanoseconds = final.tv_nsec - inicio.tv_nsec;
-			elapsed = seconds + nanoseconds * 1e - 9;
-			proceso_a_ejecutar->ultima_rafaga = elapsed;
-			
-			log_warning(logger_kernel, "La última instrucción ejecutada fue YIELD");
-			pthread_mutex_lock(&mutex_ready);
-			proceso->pcb->estado = READY;
-			list_add(cola_ready, proceso);
-			pthread_mutex_unlock(&mutex_ready);
-
-			sem_post(&sem_ready);
-			break;
-		case EXIT:
-			//Finalización del proceso.
-			pthread_mutex_lock(&mutex_exit);
-			proceso->pcb->estado = EXT;
-			list_add(cola_exit, proceso);
-			pthread_mutex_unlock(&mutex_exit);
-
-			log_info(logger_kernel, "PCB id[%d] sale de EXEC y entra a EXIT", proceso->pcb->pid);
-			sem_post(&sem_exit); //despierta el proceso que saca a los procesos del sistema
-			sem_post(&sem_ready);
-
-			log_warning(logger_kernel, "La última instrucción ejecutada fue EXIT");
-			break;
-		default:
-			log_error(logger_kernel, "Instrucción desconocida, ocurrió un error");
-			break;
-		}
-	}
-
 }
 
 t_pcb* crear_estructura_pcb(char* instrucciones) {
@@ -244,7 +93,7 @@ t_pcb* crear_estructura_pcb(char* instrucciones) {
 	un_pcb->instrucciones = string_duplicate(instrucciones);
 	un_pcb->pc = 0;
 	un_pcb->estado = NEW;
-	un_pcb->tamanio = tamanio_proceso;
+	un_pcb->tamanio = tamanio_proceso; //TODO este tiene que ser el tamaño de los segmentos, quizás no hace falta agregarlos ahora
 	un_pcb->registros = registros_iniciados;
 	un_pcb->rafaga_estimada = config_kernel.estimacion_inicial;
 	un_pcb->rafaga_anterior = 0;
@@ -257,45 +106,6 @@ t_pcb* crear_estructura_pcb(char* instrucciones) {
 	return un_pcb;
 }
 
-void estado_exit(void) {
-
-	while(1) {
-		sem_wait(&sem_exit);
-		t_proceso* proceso;
-
-		pthread_mutex_lock(&mutex_exit);
-		proceso = list_remove(cola_exit, 0);
-		pthread_mutex_unlock(&mutex_exit);
-
-		log_info(logger_kernel, "[EXIT] PCB id[%d] sale de Exit y finaliza el proceso", proceso->pcb->pid);
-
-		//Liberar las estructuras del proceso en memoria
-		//enviar_pcb(socket_memoria, proceso->pcb);
-		//log_info(logger_kernel, "Enviando pcb a memoria para liberar estructuras
-		//op_code codigo = esperar_respuesta_memoria(socket_memoria);
-		//if(codigo != ESTRUCTURAS_LIBERADAS) {
-		//	log_error(logger_kernel, "No se pudo eliminar de memoria a PCB id[%d]", proceso->pcb->pid);
-		//}
-
-		avisar_a_modulo(proceso->socket, FINALIZAR_CONSOLA);
-
-		log_info(logger_kernel, "Ordenando a Consola la finalización del proceso");
-
-		eliminar_pcb(proceso->pcb);
-		free(proceso);
-		//sem_post(&sem_grado_multiprogramacion);
-	}
-}
-
-void eliminar_pcb(t_pcb pcb) {
-	//list_destroy_and_destroy_elements(pcb->segmentos, free);
-	//TODO seguir destruyendo las eventuales listas de la pcb.
-}
-
-void iniciar_conexion_cpu(char* ip_cpu, char* puerto_cpu) {
-	socket_cpu = crear_conexion(logger_kernel, "KERNEL", ip_cpu, puerto_cpu);
-}
-
 void agregar_proceso_a_new(t_proceso* proceso) {
 
 	pthread_mutex_lock(&mutex_new);
@@ -304,12 +114,11 @@ void agregar_proceso_a_new(t_proceso* proceso) {
 
 	log_info(logger_kernel, "PCB id[%d] ingresa a NEW", proceso->pcb->pid);
 
-	mostrar_cola_new(cola_new);
+	mostrar_cola(cola_new);
 	pthread_mutex_unlock(&mutex_new);
 
 	//ATENCAO EMPIEZO A TIRAR MAGIA Y FILOSOFAR CREO QUE ES POR ACA
 	sem_post(&sem_admitir);
-
 }
 
 void admitir_procesos_a_ready(void) { //hilo
@@ -335,114 +144,38 @@ void admitir_procesos_a_ready(void) { //hilo
 
 		sem_post(&sem_ready);
 	}
-
 }
 
-void estado_ready(void) {
-
-	while(1) {
-		sem_wait(&sem_ready);
-
-		pthread_mutex_lock(&mutex_ready);
-		if(list_is_empty(cola_ready)) { //Si la lista de ready está vacía vuelve y vuelve a entrar al while hasta que haya procesos.
-			pthread_mutex_unlock(&mutex_ready);
-			continue;
-		}
-		pthread_mutex_unlock(&mutex_ready);
-
-		t_proceso* siguiente_proceso = obtener_proceso_cola_ready();
-
-		pthread_mutex_lock(&mutex_exec);
-		list_add(cola_exec, siguiente_proceso);
-		pthread_mutex_unlock(&mutex_exec);
-
-		log_info(logger_kernel, "PCB id[%d] ingresa a EXECUTE", siguiente_proceso->pcb->pid);
-		sem_post(&sem_exec);
-	}
-
-}
-
-void estado_block_io(void) {
-
-	while(1) {
-		sem_wait(&sem_block_io);
-
-		pthread_mutex_lock(&mutex_block_io);
-		t_proceso* proceso = list_remove(cola_block_io, 0);
-		pthread_mutex_unlock(&mutex_block_io);
-
-		usleep(proceso->tiempo_bloqueo);
-
-		pthread_mutex_lock(&mutex_ready);
-		list_add(cola_ready, proceso);
-		pthread_mutex_unlock(&mutex_ready);
-
-		sem_post(&sem_ready);
-	}
-}
-
-void mostrar_cola_new(t_list* lista) {
-
-	log_warning(logger_kernel, "PCBs en lista NEW:");
+void mostrar_cola(t_list* lista) {
 
     for (int j = 0; j < list_size(lista); j++){
     	t_proceso* proceso = list_get(lista, j);
-        log_info(logger_kernel,"PCB ID: %d",proceso->pcb->pid);
+        log_info(logger_kernel,"PCB id[%d]",proceso->pcb->pid);
     }
 }
 
-t_proceso* obtener_proceso_cola_ready(void) {
+//-----------------------------------------------------FUNCIONES EN DESUSO--------------------------------------------------------
 
-	t_proceso* proceso = malloc(sizeof(t_proceso));
+void iniciar_conexion_cpu(char* ip_cpu, char* puerto_cpu) { //ESTA FUNCIÓN YA NO SE USA, QUEDA POR LAS DUDAS. LO NECESARIO YA ESTÁ EN EL MAIN
+	socket_cpu = crear_conexion(logger_kernel, "KERNEL", ip_cpu, puerto_cpu);
+}
 
-	switch(config_kernel.algoritmo_planificacion){
-	case FIFO:
-		proceso = list_remove(cola_ready, 0);
-		log_info(logger_kernel, "PCB id[%d] saliendo de Cola de Ready por FIFO para ejecutar.", proceso->pcb->pid);
-		break;
-	case HRRN:
-		aplicar_hrrn();
-		proceso = list_remove(cola_ready, 0);
-		log_info(logger_kernel, "PCB id[%d] saliendo de Cola de Ready por HRRN para ejecutar.", proceso->pcb->pid);
-		break;
-	default:
-		log_error(logger_kernel, "Error al elegir un proceso de la cola de Ready para ejecutar");
-		break;
+void agregar_proceso_a_ready(void) { //ESTA FUNCIÓN YA NO LA USO, QUEDA POR LAS RISAS (POR LAS DUDAS)
+
+	if(list_size(cola_ready) <= config_kernel.grado_multiprogramacion) { // TODO Esto después tiene que ser implementado con semáforos contadores
+		t_proceso* proceso_auxiliar = malloc(sizeof(t_proceso));
+		proceso_auxiliar = list_remove(cola_new, 0);
+		list_add(cola_ready, proceso_auxiliar);
+
+		log_info(logger_kernel, "PCB id[%d] agregado a Cola de Ready", proceso_auxiliar->pcb->pid);
+		//free(proceso_auxiliar);
 	}
 
-	return proceso;
-}
+	log_info(logger_kernel, "Cola de Ready: ");
 
-void aplicar_hrrn(void) {
-
-	list_iterate(cola_ready, (void*) calcular_response_ratio);
-	list_iterate(cola_ready, (void*) mostrar_response_ratio);
-
-	list_sort(cola_ready, (void*) comparador_response_ratio);
-}
-
-void calcular_response_ratio(t_proceso* proceso) {
-	//ATENCIÓN CON LO QUE VIENE ES UNA MAGIC JOHNSON, DESPUÉS INDAGAR BIEN TODO SOBRE FUNCIONES DE CLOCK
-	struct timespec end;
-	clock_gettime(CLOCK_REALTIME, &end);
-
-	long seconds = end.tv_sec - proceso->pcb->llegada_ready.tv_sec;
-	long nanoseconds = end.tv_nsec - proceso->pcb->llegada_ready.tv_sec;
-	double elapsed = seconds + nanoseconds * 1e-9;
-
-	proceso->pcb->tiempo_espera = elapsed; //TODO poner tiempo de llegada a ready en el hilo correspondiente estado_ready y estado_block
-	proceso->pcb->response_ratio = 1 + (proceso->pcb->tiempo_espera / proceso->pcb->rafaga_estimada);
-}
-
-bool comparador_response_ratio(t_proceso* un_proceso, t_proceso* otro_proceso) {
-	return un_proceso->pcb->response_ratio >= otro_proceso->pcb->response_ratio;
-}
-
-void mostrar_response_ratio(t_proceso* un_proceso) {
-	log_info(logger_kernel, "PCB id[%d] tiene un RR de: %f", un_proceso->pcb->response_ratio);
-}
-
-void calcular_estimacion(t_proceso* un_proceso) {
-	double nueva_rafaga = (config_kernel.alfa_hrrn * un_proceso->pcb->rafaga_estimada) + ((un_proceso->pcb->ultima_rafaga) * (1 - config_kernel.alfa_hrrn));
-	un_proceso->pcb->rafaga_estimada = nueva_rafaga;
+	for(int i = 0; i < list_size(cola_ready); i++) {
+		t_proceso* un_proceso = malloc(sizeof(t_proceso)) ;
+		un_proceso = list_get(cola_ready, i);
+		log_info(logger_kernel, "PCB id[%d]", un_proceso->pcb->pid);
+	}
 }
