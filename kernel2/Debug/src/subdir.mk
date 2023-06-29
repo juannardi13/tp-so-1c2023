38ef851c506f2ee2518ad03fa2e708649bec095d
juannardi13 <jnardi@frba.utn.edu.ar>
2023-06-29 22:46:08 +0000

    COMPILAAAA

--- kernel2/Debug/src/subdir.mk
+++ kernel2/Debug/src/subdir.mk
@@ -9,6 +9,7 @@ C_SRCS += \
 ../src/hilos_kernel2.c \
 ../src/kernel2.c \
 ../src/planificadores.c \
+../src/recursos.c \
 ../src/serializacion_y_deserializacion.c \
 ../src/utils_kernel2.c 
 
@@ -18,6 +19,7 @@ C_DEPS += \
 ./src/hilos_kernel2.d \
 ./src/kernel2.d \
 ./src/planificadores.d \
+./src/recursos.d \
 ./src/serializacion_y_deserializacion.d \
 ./src/utils_kernel2.d 
 
@@ -27,6 +29,7 @@ OBJS += \
 ./src/hilos_kernel2.o \
 ./src/kernel2.o \
 ./src/planificadores.o \
+./src/recursos.o \
 ./src/serializacion_y_deserializacion.o \
 ./src/utils_kernel2.o 
 
@@ -43,7 +46,7 @@ src/%.o: ../src/%.c src/subdir.mk
 clean: clean-src
 
 clean-src:
-	-$(RM) ./src/conexion_kernel_cpu2.d ./src/conexion_kernel_cpu2.o ./src/estados.d ./src/estados.o ./src/hilos_kernel2.d ./src/hilos_kernel2.o ./src/kernel2.d ./src/kernel2.o ./src/planificadores.d ./src/planificadores.o ./src/serializacion_y_deserializacion.d ./src/serializacion_y_deserializacion.o ./src/utils_kernel2.d ./src/utils_kernel2.o
+	-$(RM) ./src/conexion_kernel_cpu2.d ./src/conexion_kernel_cpu2.o ./src/estados.d ./src/estados.o ./src/hilos_kernel2.d ./src/hilos_kernel2.o ./src/kernel2.d ./src/kernel2.o ./src/planificadores.d ./src/planificadores.o ./src/recursos.d ./src/recursos.o ./src/serializacion_y_deserializacion.d ./src/serializacion_y_deserializacion.o ./src/utils_kernel2.d ./src/utils_kernel2.o
 
 .PHONY: clean-src
 
