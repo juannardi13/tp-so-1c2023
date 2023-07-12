39234958668c3af7c87c6a455e40077db3945ba5
juannardi13 <jnardi@frba.utn.edu.ar>
2023-07-12 07:19:48 +0000

    git ignore

--- cpu2/Debug/src/subdir.mk
+++ cpu2/Debug/src/subdir.mk
@@ -4,15 +4,27 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/ciclo_instrucciones.c \
 ../src/cpu2.c \
+../src/ejecucion_instrucciones.c \
+../src/inicializacion_cpu.c \
+../src/serializacion.c \
 ../src/utils_cpu2.c 
 
 C_DEPS += \
+./src/ciclo_instrucciones.d \
 ./src/cpu2.d \
+./src/ejecucion_instrucciones.d \
+./src/inicializacion_cpu.d \
+./src/serializacion.d \
 ./src/utils_cpu2.d 
 
 OBJS += \
+./src/ciclo_instrucciones.o \
 ./src/cpu2.o \
+./src/ejecucion_instrucciones.o \
+./src/inicializacion_cpu.o \
+./src/serializacion.o \
 ./src/utils_cpu2.o 
 
 
@@ -28,7 +40,7 @@ src/%.o: ../src/%.c src/subdir.mk
 clean: clean-src
 
 clean-src:
-	-$(RM) ./src/cpu2.d ./src/cpu2.o ./src/utils_cpu2.d ./src/utils_cpu2.o
+	-$(RM) ./src/ciclo_instrucciones.d ./src/ciclo_instrucciones.o ./src/cpu2.d ./src/cpu2.o ./src/ejecucion_instrucciones.d ./src/ejecucion_instrucciones.o ./src/inicializacion_cpu.d ./src/inicializacion_cpu.o ./src/serializacion.d ./src/serializacion.o ./src/utils_cpu2.d ./src/utils_cpu2.o
 
 .PHONY: clean-src
 
