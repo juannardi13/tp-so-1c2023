07071d3837965fdac5f0d12f2d08257aed105381
camivinciguerra <cvinciguerra@frba.utn.edu.ar>
2023-07-20 03:02:48 +0000

    Mejoras atender cpu

--- memoria2/src/operaciones.c
+++ memoria2/src/operaciones.c
@@ -110,99 +110,93 @@ void atender_CPU(int *cpu_fd){
 	int socket_cliente = *cpu_fd;
 
 	while(1){
+		int cod_op = recibir_operacion(socket_cliente);
+
+		/*
 		t_paquete* paquete = malloc(sizeof(t_paquete));
 		paquete->buffer = malloc(sizeof(t_buffer));
 		recv(socket_cliente, &(paquete->codigo_operacion), sizeof(op_code), MSG_WAITALL);
 		recv(socket_cliente, &(paquete->buffer->stream_size), sizeof(int), 0);
 		paquete->buffer->stream = malloc(paquete->buffer->stream_size);
 		recv(socket_cliente, paquete->buffer->stream, paquete->buffer->stream_size, 0);
+		*/
 
 		int direccion_fisica_buscada = malloc(sizeof(int));
 		int tamanio_valor = malloc(sizeof(int));
 		char* valor_a_asignar = malloc(sizeof(t_registros));
 
-		switch(paquete->codigo_operacion){
+		switch(cod_op){
 		case LEER_DE_MEMORIA :
-			// RECIBO LA DIRECCION FISICA BUSCADA POR CPU, BUSCO SU VALOR ASOCIADO Y LO ENVIO A CPU
-			void *stream1 = paquete->buffer->stream;
-			memcpy(&(direccion_fisica_buscada), stream1, sizeof(int));
-			stream1 += sizeof(int);
-			memcpy(&(tamanio_valor), stream1, sizeof(int));
-			stream1 += sizeof(int);
-			buscar_valor_enviar_CPU(direccion_fisica_buscada, tamanio, socket_cliente);
+			recv_leer_de_memoria(socket_cliente);
+			break;
 
 
 		case ESCRIBIR_EN_MEMORIA :
-			// RECIBO LA DIRECCION FISICA Y EL VALOR A ESCRIBIR EN ELLA
-			void *stream2 = paquete->buffer->stream;
-			memcpy(&(direccion_fisica_buscada), stream2, sizeof(int));
-			stream2 += sizeof(int);
-			memcpy(&(tamanio_valor), stream2, sizeof(int));
-			stream2 += sizeof(int);
-			memcpy(&(valor_a_asignar), stream2, sizeof(t_registros));
-			stream2 += sizeof(t_registros);
-			escribir_valor_en_direccion_fisica(direccion_fisica_buscada, tamanio_valor, valor_a_asignar);
+			recv_escribir_en_memoria(socket_cliente);
+			break;
 		}
-		free(paquete->buffer->stream);
-		free(paquete->buffer);
-		free(paquete);
 	}
 }
 
-// FUNCIONES SIN DESARROLLAR TODAVIA
+
+// FUNCIONES AUXILIARES - Gero, si queres las pasamos a otro archivo
+void recv_leer_de_memoria(int socket_cliente){
+
+	int size;
+	int offset = 0;
+
+	void *buffer = recibir_buffer(&size, socket_cliente);
+
+	int direccion_fisica = deserializar_int(buffer, &offset);
+	int tamanio = deserializar_int(buffer, &offset);
+
+	buscar_valor_enviar_CPU(direccion_fisica, tamanio, socket_cliente);
+	log_info(logger, "Se busca el valor de la direccion fisica: %d", direccion_fisica);
+
+	free(buffer);
+}
+
+void recv_escribir_en_memoria(int socket_cliente){
+	int size;
+	int offset = 0;
+
+	void *buffer = recibir_buffer(&size, socket_cliente);
+
+	int direccion_fisica = deserializar_int(buffer, &offset);
+	int tamanio = deserializar_int(buffer, &offset);
+	char* valor = deserializar_string(buffer, &offset);
+
+	escribir_valor_en_direccion_fisica(direccion_fisica, tamanio, valor);
+	log_info(logger, "Se escribe el valor de la direccion fisica: %d", direccion_fisica);
+
+	free(buffer);
+}
+
+
 
 void buscar_valor_enviar_CPU(int direccion_fisica_buscada, int tamanio, int socket_cliente){
-	// Tenemos que ver como buscar una direccion fisica
-	// Si encuentra la direccion fisica, debe devolverle a cpu el valor encontrado como un paquete
 
-	t_buffer *buffer = malloc(sizeof(t_buffer));
-	t_paquete* paquete_valor = malloc(sizeof(t_paquete));
+	char* valor_buscado;
 
-	char* valor_buscado; // le deberia asignar el valor que encontre
-
-	if(memcpy(valor_buscado, memoria + direccion_fisica_buscada, tamanio) != NULL){ // if encuentra direccion fisica
-		msleep(config_memoria.retardo_memoria);
+	if(&(memoria + direccion_fisica_buscada) != NULL){
+		msleep(config_memoria.retardo_memoria); // Cada vez que accede al espacio memoria debe retrasarse segun config
 		memcpy(valor_buscado, memoria + direccion_fisica_buscada, tamanio);
-
-		buffer->stream_size = sizeof(tamanio);
-		void *stream = malloc(buffer->stream_size);
-		int offset = 0;
-		memcpy(stream + offset, &tamanio, sizeof(int));
-		offset += sizeof(int);
-		memcpy(stream + offset, &valor_buscado, sizeof(tamanio));
-		offset += tamanio;
-
-		buffer->stream = stream;
-
-		paquete_valor->codigo_operacion = LEIDO;
-		paquete_valor->buffer = buffer;
+		t_paquete paquete = crear_paquete(LEIDO);
+		agregar_int_a_paquete(paquete, tamanio);
+		agregar_string_a_paquete(paquete, valor_buscado, tamanio+1);
+		enviar_paquete(paquete, socket_cliente);
+		log_info(logger, "Se envia el valor: %s", valor_buscado);
 	}
 	else{
-		paquete_valor->codigo_operacion = NO_LEIDO;
-		paquete_valor->buffer->stream = NULL;
-		paquete_valor->buffer->stream_size = 0;
-
+		t_paquete paquete = crear_paquete(NO_LEIDO);
+		enviar_paquete(paquete, socket_cliente);
 	}
-
-	void* a_enviar = malloc(buffer->stream_size + sizeof(int) + sizeof(int));
-	int desplazamiento = 0;
-
-	agregar_a_stream(a_enviar, &desplazamiento, &(paquete_valor->codigo_operacion), sizeof(int));
-	agregar_a_stream(a_enviar, &desplazamiento, &(paquete_valor->buffer->stream_size), sizeof(int));
-	agregar_a_stream(a_enviar, &desplazamiento, paquete_valor->buffer->stream, paquete_valor->buffer->stream_size);
-
-	send(socket_cliente, a_enviar, buffer->stream_size + sizeof(int) + sizeof(int), 0);
-
-	free(a_enviar);
-	free(paquete_valor->buffer->stream);
-	free(paquete_valor->buffer);
-	free(paquete_valor);
-
 }
 
+
+
+
 void escribir_valor_en_direccion_fisica(int direccion_fisica, int tamanio_valor, char* valor){
-	// Buscar direccion fisica, le asigna valor que me paso CPU
-	// Ver que pasa si no la encuentra !!!
 
 	log_info(logger, "Dirección física: %d", direccion_fisica);
 	log_info(logger, "Valor Registro: %s", valor);
@@ -217,10 +211,12 @@ void escribir_valor_en_direccion_fisica(int direccion_fisica, int tamanio_valor,
 
 	free(aux);
 	free(valor);
-	log_info(logger, "OK, se escribio el valor en memoria");// agreamos log para enviar mensaje de ok
+	log_info(logger, "OK, se escribio el valor en memoria");
 }
 
+/*
 int agregar_a_stream(void* stream, int* offset, void* src, int size) {
 	memcpy(stream + *offset, src, size);
 	*offset +=size;
 }
+*/
