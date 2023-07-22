c12fdccf51912302c5cccb853181148609a42865
juannardi13 <jnardi@frba.utn.edu.ar>
2023-07-22 08:09:47 +0000

    Creación de nuevas funciones para manejar a la conexión con el FileSystem

--- kernel2/src/archivos.c
+++ kernel2/src/archivos.c
@@ -92,8 +92,129 @@ void funcion_archivo(char* key, void* elemento) {
 		proceso->llegada_ready = get_time();
 		pthread_mutex_unlock(&mutex_ready);
 
-		sem_post(&sem_ready);
+		//sem_post(&sem_ready);
 	}
 
 	pthread_mutex_unlock(archivo->mutex_instancia_usada);
+}
+
+void liberar_entrada_archivo(t_archivo* archivo) {
+	pthread_mutex_lock(archivo->mutex_instancia_usada);
+	archivo->instancias_usadas--;
+	pthread_mutex_unlock(archivo->mutex_instancia_usada);
+
+	sem_post(&sem_archivos);
+}
+
+void actualizar_puntero_archivo_proceso(t_proceso* proceso, char* nombre, int nuevo_puntero) {
+
+	bool buscar_archivo(void* un_archivo) {
+		char* nombre_archivo = (char*) un_archivo;
+		return string_equals_ignore_case(nombre_archivo, nombre);
+	}
+
+	t_archivo_proceso* archivo = list_find(proceso->pcb->tabla_archivos, buscar_archivo);
+
+	log_info(logger_kernel, "Actualización del puntero a <%d> del archivo: <%s>", nuevo_puntero, archivo->nombre);
+
+	archivo->puntero = nuevo_puntero;
+}
+
+op_code consultar_existencia_archivo_a_fs(char* nombre) {
+	op_code respuesta;
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+
+	int tamanio_nombre_archivo = strlen(nombre) + 1;
+
+	buffer->stream_size = sizeof(int)
+			+ tamanio_nombre_archivo;
+
+	void* stream = malloc(buffer->stream_size);
+	int offset = 0;
+
+	memcpy(stream + offset, &tamanio_nombre_archivo, sizeof(int));
+	offset += sizeof(int);
+	memcpy(stream + offset, nombre, tamanio_nombre_archivo);
+	offset += tamanio_nombre_archivo;
+
+	buffer->stream = stream;
+
+	paquete->codigo_operacion = F_OPEN;
+	paquete->buffer = buffer;
+
+	void* a_enviar = malloc(buffer->stream_size + sizeof(int) + sizeof(int));
+	int desplazamiento = 0;
+
+	agregar_a_stream(a_enviar, &desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+	agregar_a_stream(a_enviar, &desplazamiento, &(paquete->buffer->stream_size), sizeof(int));
+	agregar_a_stream(a_enviar, &desplazamiento, paquete->buffer->stream, paquete->buffer->stream_size);
+
+	send(socket_filesystem, a_enviar, buffer->stream_size + sizeof(int) + sizeof(int));
+
+	free(a_enviar);
+	free(stream);
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+
+	recv(socket_filesystem, &respuesta, sizeof(op_code), MSG_WAITALL); //Tendria que ser el op_code EXISTE o NO_EXISTE
+
+	switch(respuesta) {
+	case EXISTE:
+		log_info(logger_kernel, "El archivo <%s> existe en FileSystem", nombre);
+		respuesta = EXISTE;
+		break;
+	case NO_EXISTE:
+		log_info(logger_kernel, "El archivo <%s> no existe en FileSystem", nombre);
+		respuesta = NO_EXISTE;
+		break;
+	default:
+		log_error(logger_kernel, "[ERROR] Error al determinar si existe <%s> en FileSystem", nombre);
+		break;
+	}
+
+	return respuesta;
+}
+
+void crear_archivo_en_fs(char* nombre) {
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	t_buffer* buffer = malloc(sizeof(t_buffer));
+
+	int tamanio_nombre_archivo = strlen(nombre) + 1;
+
+	buffer->stream_size = sizeof(int)
+			+ tamanio_nombre_archivo;
+
+	void* stream = malloc(buffer->stream_size);
+	int offset = 0;
+
+	memcpy(stream + offset, &tamanio_nombre_archivo, sizeof(int));
+	offset += sizeof(int);
+	memcpy(stream + offset, nombre, tamanio_nombre_archivo);
+	offset += tamanio_nombre_archivo;
+
+	buffer->stream = stream;
+
+	paquete->codigo_operacion = CREAR_ARCHIVO;
+	paquete->buffer = buffer;
+
+	void* a_enviar = malloc(buffer->stream_size + sizeof(int) + sizeof(int));
+	int desplazamiento = 0;
+
+	agregar_a_stream(a_enviar, &desplazamiento, &(paquete->codigo_operacion), sizeof(int));
+	agregar_a_stream(a_enviar, &desplazamiento, &(paquete->buffer->stream_size), sizeof(int));
+	agregar_a_stream(a_enviar, &desplazamiento, paquete->buffer->stream, paquete->buffer->stream_size);
+
+	send(socket_filesystem, a_enviar, buffer->stream_size + sizeof(int) + sizeof(int));
+
+	free(a_enviar);
+	free(stream);
+	free(paquete->buffer->stream);
+	free(paquete->buffer);
+	free(paquete);
+
+	//TODO creo que solo le digo que lo cree y listo, no creo que necesite esperar una respuesta
 }
